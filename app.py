f490189b5516ce0e9479d75e7e62e51c88920805
zanbary34 <102601986+zanbary34@users.noreply.github.com>
2022-06-11 17:41:27 +0300

    11/06

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect,render_template
+app = Flask(__name__)
+@app.route('/')
+@app.route('/home')
+def index_func():
+    return render_template('Home.html' , user_name='')
+
+@app.route('/contact')
+def about_page():
+    user_info = {'name': "Jack" ,"second_name": "Sparo"}
+    degrees =  []
+    hobbies =()
+    return render_template('Contact.html',user_info = user_info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
